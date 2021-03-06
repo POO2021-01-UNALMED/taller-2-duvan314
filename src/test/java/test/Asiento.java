7669ada6ad62c4f8e9cc4543a42e9499f95f60d3
Duvan Camilo Manrique Vargas <43693076+duvan314@users.noreply.github.com>
2021-03-05 22:30:13 -0500

    Creando clases

---
+++ src/test/java/test/Asiento.java
@@ -0,0 +1,8 @@
+package test;
+
+public class Asiento {
+    String color;
+    int precio;
+    int registro;
+
+}
---
+++ src/test/java/test/Motor.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Motor {
+    int numeroCilindros;
+    String tipo;
+    int registro;
+
+
+}
