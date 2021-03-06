fa5fc6359c7e1459357a21fb76eeb65267ebae2a
Duvan Camilo Manrique Vargas <43693076+duvan314@users.noreply.github.com>
2021-03-06 08:14:33 -0500

    CLASES

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -5,12 +5,28 @@ public class Asiento {
     int precio;
     int registro;
 
-    void cambiarColor(String color) {
-        switch (color) {   // un if interesante
-            case "verde", "amarillo", "negro", "blanco" -> this.color = color;
-            default -> System.out.println("Color no Valido");
-        }
+//    void cambiarColor(String color) {
+//        switch (color) {   // un if interesante
+//            case "verde", "amarillo", "negro", "blanco" -> this.color = color;
+//            default -> System.out.println("Color no Valido");
+//        }
+//
+//    }
 
+
+
+    void cambiarColor(String color) {
+        if (color.equals("rojo")) {
+            this.color = color;
+        } else if (color.equals("verde")) {
+            this.color = color;
+        }  else if (color.equals("amarillo")) {
+            this.color = color;
+        }  else if (color.equals("blanco")) {
+            this.color = color;
+        } else if (color.equals("negro")){
+            this.color = color;
+        }
     }
 
 
